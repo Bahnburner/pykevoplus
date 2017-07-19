f776143d9e4a857a78c605a18bb26d706f76048b
Bahnburner <tcaulfld@gmail.com>
2017-07-19 17:43:16 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,15 +2,15 @@ from setuptools import setup
 import os
 
 setup(
-    name = "pykevoplus",
+    name = "pykevoplus3",
     version = "1.0.1",
-    author = "Carl Seelye",
-    author_email = "cseelye@gmail.com",
+    author = "Tristan Caulfield",
+    author_email = "tcaulfld@gmail.com",
     description = "Control Kwikset Kevo locks",
     license = "MIT",
     keywords = "kevo kwikset",
-    packages = ["pykevoplus"],
-    url = "https://github.com/cseelye/pykevoplus",
+    packages = ["pykevoplus3"],
+    url = "https://github.com/bahnburner/pykevoplus3",
     long_description = open(os.path.join(os.path.dirname(__file__), "README.rst")).read(),
     install_requires = [
         "requests",
